d2b65658b60cbc08e179847b99935f55a11fdb78
s.notevskiy <s.notevskiy@gmail.com>
2019-10-28 23:31:09 +0200

    добавлен тест наличия h1 на каждой странице пунктов левого меню

--- test_left_menu.py
+++ test_left_menu.py
@@ -1,11 +1,12 @@
-from selenium import webdriver
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-
 
 def test_left_menu(app):
+    wd = app.wd
     app.login_as_admin()
-    pass
+    left_panel = wd.find_element_by_id('box-apps-menu')
+    left_panel_tabs = left_panel.find_elements_by_id('app-')
+    for i in left_panel_tabs:
+        num = left_panel_tabs.index(i)
+        wd.find_elements_by_xpath('*//ul[@id = "box-apps-menu"]/li')[num].click()
+        assert wd.find_element_by_tag_name('h1').is_displayed()
 
 
