bc3786adc0a59def3389f6c76dd8770f9bc52083
s.notevskiy <s.notevskiy@gmail.com>
2019-10-28 23:56:34 +0200

    добавлена проверка h1 для вложенных пунктов левого меню

--- test_left_menu.py
+++ test_left_menu.py
@@ -2,11 +2,20 @@
 def test_left_menu(app):
     wd = app.wd
     app.login_as_admin()
-    left_panel = wd.find_element_by_id('box-apps-menu')
-    left_panel_tabs = left_panel.find_elements_by_id('app-')
+    left_panel_tabs = wd.find_elements_by_css_selector('#box-apps-menu #app-')
     for i in left_panel_tabs:
         num = left_panel_tabs.index(i)
-        wd.find_elements_by_xpath('*//ul[@id = "box-apps-menu"]/li')[num].click()
+        tab_locator = '*//ul[@id = "box-apps-menu"]/li'
+        wd.find_elements_by_xpath(tab_locator)[num].click()
         assert wd.find_element_by_tag_name('h1').is_displayed()
+        inserted_tabs = wd.find_elements_by_xpath(tab_locator)[num].find_elements_by_css_selector('.docs li')
+        if len(inserted_tabs) > 0:
+            for n in inserted_tabs:
+                index = inserted_tabs.index(n)
+                wd.find_elements_by_xpath(tab_locator)[num].find_elements_by_css_selector(
+                    '.docs li')[index].click()
+                assert wd.find_element_by_tag_name('h1').is_displayed()
+
+
 
 
