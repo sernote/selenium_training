f59a6ff8ab226f7dc7db08f8e68652a2e6b4e0ba
s.notevskiy <s.notevskiy@gmail.com>
2019-11-03 22:51:18 +0200

    сокращен локатор для поиска продуктов

--- test_stickers.py
+++ test_stickers.py
@@ -1,7 +1,7 @@
 
 def test_all_products_have_one_sticker(app):
     app.go_to_main_page()
-    products = app.wd.find_elements_by_xpath('*//li[@class = "product column shadow hover-light"]')
+    products = app.wd.find_elements_by_css_selector('.product')
     for product in products:
         assert len(product.find_elements_by_xpath('.//div[contains(@class, "sticker")]')) == 1
 
